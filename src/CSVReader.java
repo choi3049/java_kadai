3ed55fd13df3aecadaac138e42c500c9e38ec1a2
choe <qudtjs3012@naver.com>
2023-02-08 23:36:30 +0900

    CSV_Writer

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,57 +1,33 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class CSVReader {
     public static void main(String[] args) {
-        String fileName = "C:\\Users\\choeb\\OneDrive\\デスクトップ\\研修\\01.課題\\2.kadai.csv";
 
-        // ファイル存在チェック
-        File file = new File(fileName);
-        if (!file.exists()) {
-            System.out.print("ファイルが存在しません");
-            return;
-        }
+
+        File file = null;
+        BufferedWriter bw = null;
+        String NEWLINE = System.lineSeparator();
 
         //　今日の日付
         String today;
         Date date = new Date();
-        today = new SimpleDateFormat("yyyy-MM-dd").format(date);
+        today = new SimpleDateFormat("yyyyMMdd").format(date);
+
+        String fileName = "C:\\Users\\choeb\\OneDrive\\デスクトップ\\研修\\01.課題\\3.kadai_"+ today +".csv";
 
         //　ファイル読み込み
-        try (FileReader fileReader = new FileReader(fileName);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] arrayStr = line.split("\n");
+        try {
+            file = new File(fileName);
+            bw = new BufferedWriter(new FileWriter(file));
 
-                // 1行ずつ読み込む
-                for (String str : arrayStr) {
-
-                    // 100文字以上の場合
-                    if (str.length() >= 100) {
-                        continue;
-                    }
-
-                    // 名前、日付変更
-                    if (str.contains("000") || str.contains("data")) {
-                        str = str.replace("000", "choe").replace("date", today);
-                    }
-
-                    // 名前大文字変更
-                    if (str.contains(":")){
-                        int a = str.indexOf(":");
-                        str = str.substring(0, a).toUpperCase() + str.substring(a).toLowerCase();
-                    }
-                    str = str.replace("#", "").trim(); // 左右空白と"#"削除
-
-                    System.out.println(str);
-                }
-            }
+            bw.write("佐藤");
+            bw.write(NEWLINE);
+            bw.write("佐藤");
+            bw.flush();
+            bw.close();
         } catch (IOException e) {
             System.out.println(e);
         }
---
+++ src/CSVWriter.java
@@ -0,0 +1,3 @@
+public class CSVWriter {
+    String fileName = "C:\\Users\\choeb\\OneDrive\\デスクトップ\\研修\\01.課題\\2.kadai.csv";
+}
